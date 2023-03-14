66bbe96ed4e978e25a52a415e4d78655fb0aafd5
Rabe Christian <rabenklar@gmx.de>
2023-03-14 12:01:53 +0100

    add formData

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,24 @@
 export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    const weatherCheck = {
+      name: data.name,
+      isForGoodWeather: data.isForGoodWeather.checked,
+    };
+    // onAddActivity(data);
+    console.log(weatherCheck);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h1>Space Weatherapp</h1>
-      <label htmFor="activity">Activity</label>
-      <input type="text" id="activity"></input>
+      <label htmlFor="activity">Activity</label>
+      <input type="text" id="activity" name="activtiy"></input>
       <label htmlFor="checkbox"></label>
-      <input type="checkbox" id="checkbox"></input>
+      <input type="checkbox" id="checkbox" name="isForGoodWeather"></input>
       <button type="submit">Submit</button>
     </form>
   );
